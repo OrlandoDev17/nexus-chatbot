026ff85714606c6426a4b7144ffa762b9fb20312
OrlandoDev17 <orlandodev1707@gmail.com>
2025-06-25 01:54:12 -0400

    comentar errores

--- src/app/api/analize/route.ts
+++ src/app/api/analize/route.ts
@@ -1,16 +1,16 @@
-import Tesseract from "tesseract.js";
+// import Tesseract from "tesseract.js";
 
-export async function POST(req: Request) {
-  const formData = await req.formData();
-  const file = formData.get("file") as File;
+// export async function POST(req: Request) {
+//   const formData = await req.formData();
+//   const file = formData.get("file") as File;
 
-  const arrayBuffer = await file.arrayBuffer();
-  const blob = new Blob([arrayBuffer]);
+//   const arrayBuffer = await file.arrayBuffer();
+//   const blob = new Blob([arrayBuffer]);
 
-  const result = await Tesseract.recognize(blob, "eng");
-  const text = result.data.text;
+//   const result = await Tesseract.recognize(blob, "eng");
+//   const text = result.data.text;
 
-  return Response.json({
-    content: text,
-  });
-}
+//   return Response.json({
+//     content: text,
+//   });
+// }
